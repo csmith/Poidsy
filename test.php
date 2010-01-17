6b766f738cfee09c4cdc3c651b29f8070bc74b00
Chris Smith <chris@dmdirc.com>
2010-01-17 04:03:09 +0000

    Add test for allow_url_fopen

--- test.php
+++ test.php
@@ -56,6 +56,7 @@
 
  echo '<tr><th colspan="2">Poidsy requirements</th></tr>';
  doTest('PHP Version', version_compare(PHP_VERSION, '5.2.0', '>='), 'Poidsy requires PHP version 5.2.0 or greater to run');
+ doTest('Allow_url_fopen', ini_get('allow_url_fopen'), 'Poidsy requires allow_url_fopen to be configured to true');
 
  echo '<tr><th colspan="2">Associate mode requirements</th></tr>';
  doTest('hash_hmac function', function_exists('hash_hmac'), 'Poidsy requires the hash_hmac function to use associate mode. It should be available in PHP 5.2.0 or greater, unless you\'ve explicitly disabled it when compiling PHP');
